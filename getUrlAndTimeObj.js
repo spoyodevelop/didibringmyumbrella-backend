37289a6b75c755b1f2bdadd120f20e6312a3827d
spoyodevelop <kjy9505@gmail.com>
2025-10-04 04:40:48 +0900

    update getUrlAndTimeObj.js to set timezone to Asia/Seoul and modify API URLs for Korea Meteorological Service

--- getUrlAndTimeObj.js
+++ getUrlAndTimeObj.js
@@ -4,7 +4,7 @@ const {
   baseTimes,
 } = require("./dateFormatting");
 const { DUMMY_CAPITAL } = require("./locations");
-require("dotenv").config();
+process.env.TZ = "Asia/Seoul";
 const KOREA_METEOROLOGICAL_API_KEY = process.env.KOREA_METEOROLOGICAL_API_KEY;
 // const { KOREA_METEOROLOGICAL_API_KEY } = require("./majorKeys");
 
@@ -15,9 +15,11 @@ function getTimeObj(usage, dataType) {
   let year, month, day, hour, minute;
 
   if (dataType === "currentData") {
-    ({ year, month, day, hour, minute } = getPastFormattedHour(0, false));
+    ({ year, month, day, hour, minute } = getPastFormattedHour(1, false));
   } else if (dataType === "pastData") {
-    ({ year, month, day, hour, minute } = getCurrentBaseDate(new Date()));
+    ({ year, month, day, hour, minute } = getCurrentBaseDate(
+      new Date(Date.now())
+    ));
   }
   return { usage, dataType, year, month, day, hour, minute };
 }
@@ -37,11 +39,11 @@ function getUrl(locationObj, timeObj) {
   const { dataType, year, month, day, hour, minute } = timeObj;
 
   if (dataType === "pastData") {
-    url = `http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=${KOREA_METEOROLOGICAL_API_KEY}&numOfRows=10&pageNo=1&base_date=${year}${month}${day}&base_time=${hour}${minute}&nx=${
+    url = `https://apihub.kma.go.kr/api/typ02/openApi/VilageFcstInfoService_2.0/getVilageFcst?authKey=${KOREA_METEOROLOGICAL_API_KEY}&numOfRows=10&pageNo=1&base_date=${year}${month}${day}&base_time=${hour}${minute}&nx=${
       convertedX ? convertedX : capitalNX
     }&ny=${convertedY ? convertedY : capitalNY}`;
   } else {
-    url = `http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?serviceKey=${KOREA_METEOROLOGICAL_API_KEY}&numOfRows=10&pageNo=1&base_date=${year}${month}${day}&base_time=${hour}${minute}&nx=${
+    url = `https://apihub.kma.go.kr/api/typ02/openApi/VilageFcstInfoService_2.0/getUltraSrtNcst?authKey=${KOREA_METEOROLOGICAL_API_KEY}&numOfRows=10&pageNo=1&base_date=${year}${month}${day}&base_time=${hour}${minute}&nx=${
       convertedX ? convertedX : capitalNX
     }&ny=${convertedY ? convertedY : capitalNY}`;
   }
